d260a2d74557797fca462dec4581644e84ba05a7
RasmusAgergaard <rasmusap@gmail.com>
2019-04-26 14:57:18 +0200

    Bubble Sort started

--- SortingAlgorithms.BL/BubbleSort.cs
+++ SortingAlgorithms.BL/BubbleSort.cs
@@ -6,7 +6,10 @@ using System.Threading.Tasks;
 
 namespace SortingAlgorithms.BL
 {
-    public class Class1
+    public class BubbleSort
     {
+       // int[] numbers = new int[100];
+
+
     }
 }
