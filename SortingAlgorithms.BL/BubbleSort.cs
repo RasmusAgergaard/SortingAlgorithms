d9d5379cefe6111526e1a14dac7369863c6a25b0
RasmusAgergaard <rasmusap@gmail.com>
2019-04-28 14:07:21 +0200

    Drawing lines

--- SortingAlgorithms.BL/BubbleSort.cs
+++ SortingAlgorithms.BL/BubbleSort.cs
@@ -8,8 +8,20 @@ namespace SortingAlgorithms.BL
 {
     public class BubbleSort
     {
-       // int[] numbers = new int[100];
+        private Random random = new Random();
+        public int[] numbers = new int[100];
 
+        public BubbleSort()
+        {
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = random.Next(100);
+            }
+        }
 
+        public void SortList()
+        {
+
+        }
     }
 }
--- SortingAlgorithms.UI/MainWindow.xaml.cs
+++ SortingAlgorithms.UI/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SortingAlgorithms.BL;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,44 @@ namespace SortingAlgorithms.UI
     /// </summary>
     public partial class MainWindow : Window
     {
+        BubbleSort bubbleSort = new BubbleSort();
+        int[] numbers;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            numbers = bubbleSort.numbers;
+
+            DrawLines();
+        }
+
+        public void DrawLines()
+        {
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                // Create a Line  
+                Line redLine = new Line();
+                redLine.X1 = i * 5;
+                redLine.Y1 = 0;
+                redLine.X2 = i * 5;
+                redLine.Y2 = numbers[i] * 3;
+
+                // Create a red Brush  
+                SolidColorBrush redBrush = new SolidColorBrush();
+                redBrush.Color = Colors.Red;
+
+                // Set Line's width and color  
+                redLine.StrokeThickness = 4;
+                redLine.Stroke = redBrush;
+
+                // Add line to the Grid.  
+                grid1.Children.Add(redLine);
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
         }
     }
 }
